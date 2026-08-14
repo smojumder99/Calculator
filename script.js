eedbffb137e6ad1534065611da31adab4bbe3cbe
Snehasish Mojumder <278628510+smojumder99@users.noreply.github.com>
2026-08-14 18:38:25 +0530

    Contain all the four basic math operators

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(num1,num2){
+return num1+num2;
+};
+
+function subtract(num1,num2){
+return num1-num2;
+};
+
+function multiply(num1,num2){
+return num1*num2;
+};
+
+function divide(num1,num2){
+return num1/num2;
+};
+
+console.log(`9+3=${add(9,3)}`);
+console.log(`9-3=${subtract(9,3)}`);
+console.log(`9*3=${multiply(9,3)}`);
+console.log(`9/3=${divide(9,3)}`);
