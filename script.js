ef1f1fbeaa856dc6889f5f79dee04151477d84a4
Snehasish Mojumder <278628510+smojumder99@users.noreply.github.com>
2026-08-14 19:14:28 +0530

    Create three Variables and an operate function

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Requirement 1
+
 function add(num1,num2){
 return num1+num2;
 };
@@ -14,7 +16,24 @@ function divide(num1,num2){
 return num1/num2;
 };
 
-console.log(`9+3=${add(9,3)}`);
-console.log(`9-3=${subtract(9,3)}`);
-console.log(`9*3=${multiply(9,3)}`);
-console.log(`9/3=${divide(9,3)}`);
+
+
+// Requirement 2
+
+let firstOperand;
+let secondOperand;
+let operator;
+
+
+
+// Requirement 3
+
+function operate(num1, num2, operator) {
+  switch (operator) {
+    case '+': return add(num1,num2);
+    case '-': return subtract(num1,num2);
+    case '*': return multiply(num1,num2);
+    case '/': return divide(num1,num2);
+    
+  }
+}
